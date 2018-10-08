4a1af585bc20ac8040797bd958d0bd4f01304489
Jon Leibham <jleibham@gmail.com>
2018-10-08 15:44:27 -0500

    added font awesome close icon

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+
+library.add( faTimes )
+
+Vue.component('fa', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
