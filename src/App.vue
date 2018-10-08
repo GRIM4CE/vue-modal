0bab30ff99bdb821770a57c2b5097a879ec2ecfd
Jon Leibham <jleibham@gmail.com>
2018-10-08 13:47:05 -0500

    init of modal component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,110 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div class="modal" :class="[{active : state}, modalClass]" v-on:click.self="closeModal">
+    <div class="modal-container">
+      <header class="modal-header" :class="{'no-header' : headerless}">
+        <slot name="header" v-if="!headerless"></slot>
+        <fa class="modal-close" icon="times" @click="closeModal()"/>
+      </header>
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  props: ['state', 'headerless', 'modalClass'],
+  data: () => {
+    return {
+      modalState: this.state
+    }
+  },
+  methods: {
+    closeModal() {
+      this.modalState = null
+      this.$emit('clicked', this.modalState)
+      document.querySelector('body').classList.remove('_lock')
+    },
+  },
+  mounted() {
+    document.querySelector('body').classList.add('_lock')
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+$white: #fff;
+$black: #231f20;
+
+@mixin tablet {
+  @media all and (min-width: 37.5rem) {
+    @content;
+  }
+}
+
+@mixin desktop {
+  @media all and (min-width: 62.5rem) {
+    @content;
+  }
+}
+
+.modal {
+  display: none;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: $white;
+  overflow: scroll;
+  z-index: 10;
+  grid-template-columns: 1fr;
+
+  &.active {
+    display: grid;
+  }
+
+  @include tablet {
+    align-content: center;
+    background-color: rgba($black, .4);
+
+    &._large {
+      align-content: flex-start;
+      .modal-container {
+        max-width: 800px;
+      }
+    }
+  }
+}
+
+.modal-container {
+  @include tablet {
+    position: relative;
+    width: 90%;
+    max-width: 450px;
+    margin: 3rem auto;
+    border-radius: 3px;
+    background-color: $white;
+  }
+}
+
+.modal-header {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  height: 2rem;
+  padding: 0 1rem;
+  background-color: $black;
+  color: $white;
+
+  &.no-header {
+    background-color: transparent;
+    color: inherit;
+  }
+}
+
+.modal-close {
+  margin-left: auto;
+  font-size: 16px;
+  cursor: pointer;
 }
 </style>
